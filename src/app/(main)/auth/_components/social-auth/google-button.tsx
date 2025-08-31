ba0fdd168cdeb5d652df56b19cccd84a926ecd27
Junyan Weng <jw3837@drexel.edu>
2025-08-30 20:13:16 -0400

    no google btn

--- src/app/(main)/auth/_components/social-auth/google-button.tsx
+++ src/app/(main)/auth/_components/social-auth/google-button.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 export function GoogleButton({ className, ...props }: React.ComponentProps<typeof Button>) {
-  return (
+  return (//unused
     <Button variant="secondary" className={cn(className)} {...props}>
       <SimpleIcon icon={siGoogle} className="size-4" />
       Continue with Google
